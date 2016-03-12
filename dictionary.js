bc19ee1bb030263db79913aed34921f7b9929b97
Yulia <yulia@Yulias-MacBook.local>
2016-03-12 13:30:16 -0500

    support for __proto__

--- dictionary.js
+++ dictionary.js
@@ -1,21 +1,39 @@
 function Dictionary (elements) {
 	this.elements = elements || {};
+	this.hasSpecialProto = false;
+	this.specialProto = undefined;
 }
 
 Dictionary.prototype.has = function(key) {
+	if (key === '__proto__') {
+		return this.hasSpecialProto;
+	}
 	return {}.hasOwnProperty.call(this.elements, key);
 }
 
 Dictionary.prototype.get = function(key) {
+		if (key === '__proto__') {
+		return this.specialProto;
+	}
 	return this.has(key) ? 
 		this.elements[key] : 
 		undefined;
 }
 
 Dictionary.prototype.set = function(key, val) {
-	this.elements[key] = val;
+	if (key === '__proto__') {
+		this.hasSpecialProto = true;
+		this.specialProto = val;
+	} else {
+		this.elements[key] = val;
+	}
 }
 
 Dictionary.prototype.remove = function(key) {
-	delete this.elements[key];
+	if (key === '__proto__') {
+		this.hasSpecialProto = false;
+		this.specialProto = undefined;
+	} else {
+		delete this.elements[key];
+	}
 }
